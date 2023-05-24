99b3b82c1c2bc05774cfd780994904333566785e
Celia Cintas <celiacintas@Celias-MacBook-Pro.local>
2023-05-24 15:24:14 +0300

    requirements and initial data intro

--- Day-1/aux.py
+++ Day-1/aux.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
+from torch._utils import _accumulate
+from torch import randperm, utils
+from torchvision import datasets, transforms
+import numpy as np
+
 
 ## Auxiliary functions for the ipynb
 
+def create_plot_window(vis, xlabel, ylabel, title):
+    return vis.line(X=np.array([1]), Y=np.array([np.nan]), opts=dict(xlabel=xlabel, ylabel=ylabel, title=title))
 
 def show_samples(images, groundtruth):
     """
@@ -32,3 +39,37 @@ def show_samples(images, groundtruth):
 
     f.tight_layout()
     return f
+
+
+class Subset(utils.data.Dataset):
+    """
+    Subset of a dataset at specified indices.
+
+    Arguments:
+        dataset (Dataset): The whole Dataset
+        indices (sequence): Indices in the whole set selected for subset
+    """
+    def __init__(self, dataset, indices):
+        self.dataset = dataset
+        self.indices = indices
+
+    def __getitem__(self, idx):
+        return self.dataset[self.indices[idx]]
+
+    def __len__(self):
+        return len(self.indices)
+
+
+def random_split(dataset, lengths):
+    """
+    Randomly split a dataset into non-overlapping new datasets of given lengths.
+
+    Arguments:
+        dataset (Dataset): Dataset to be split
+        lengths (sequence): lengths of splits to be produced
+    """
+    if sum(lengths) != len(dataset):
+        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
+
+    indices = randperm(sum(lengths))
+    return [Subset(dataset, indices[offset - length:offset]) for offset, length in zip(_accumulate(lengths), lengths)]
